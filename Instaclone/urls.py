814b46ecd155a4ed12316beb24a6082ac444b255
Greg <blackrocket.47@gmail.com>
2020-02-08 20:57:03 +0300

    Added urls in the app

--- Instaclone/urls.py
+++ Instaclone/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from instagram.views import PostLikeToggle, PostLikeAPIToggle
+from Instaclone.views import PostLikeToggle, PostLikeAPIToggle
 from . import views
 
 urlpatterns = [
